982b839edeb6953b721e1b6d2799a24fd8f9f896
sahidrajaansari <sahidrajaansari7@gmail.com>
2024-05-24 19:08:07 +0530

    Created User Controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,7 +1,9 @@
-import { response } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../model/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
   // validation - not empty
@@ -12,8 +14,15 @@ const registerUser = asyncHandler(async (req, res) => {
   // remove password and refresh token field from response
   // check for user creation
   // return res
-
   const { fullName, email, password, username } = req.body;
+
+  console.table({
+    fullName,
+    email,
+    password,
+    username,
+  });
+
   if (
     [fullName, email, password, username].some((field) => field?.trim() === "")
   ) {
@@ -25,6 +34,46 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "User with email or username already exists");
   }
 
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  console.log(avatarLocalPath);
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is Required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is Required");
+  }
+
+  const user = User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    username: username.toLowerCase(),
+    password,
+  });
+
+  console.log(user);
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  console.log(createdUser);
+
+  if (createdUser) {
+    throw new ApiError(500, "Something Went Wrong while Regestring the user");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdUser, "User Registration Successfully"));
   // const avatarLocalPath = req.files?.avatar[0]?.path;
   // let coverImageLocalPath;
   // if (
@@ -34,8 +83,6 @@ const registerUser = asyncHandler(async (req, res) => {
   // ) {
   //   coverImageLocalPath = req.files.coverImage[0].path;
   // }
-
-  
 });
 
 export { registerUser };
