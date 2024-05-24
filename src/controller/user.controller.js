bd3dd7e9a8c212c2befecabff96cc1aadc2e754b
sahidrajaansari <sahidrajaansari7@gmail.com>
2024-05-24 18:14:39 +0530

    Added Till Video no 14

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,14 +1,41 @@
 import { response } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
+import { User } from "../model/user.model.js";
+const registerUser = asyncHandler(async (req, res) => {
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists: username, email
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
 
-const registerUser = asyncHandler((req, res) => {
   const { fullName, email, password, username } = req.body;
   if (
     [fullName, email, password, username].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required");
   }
+
+  const exsistedUser = await User.findOne({ $or: [{ username }, { email }] });
+  if (exsistedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  // const avatarLocalPath = req.files?.avatar[0]?.path;
+  // let coverImageLocalPath;
+  // if (
+  //   req.files &&
+  //   Array.isArray(req.files.coverImage) &&
+  //   req.files.coverImage.length > 0
+  // ) {
+  //   coverImageLocalPath = req.files.coverImage[0].path;
+  // }
+
+  
 });
 
 export { registerUser };
